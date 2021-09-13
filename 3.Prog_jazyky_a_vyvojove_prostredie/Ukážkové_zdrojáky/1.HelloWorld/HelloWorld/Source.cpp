1c164f37609d6b0b433a5af7f325c9046d3b2544
YoungLocal <surzo18@gmail.com>
2021-09-13 11:51:29 +0200

    Updated all sources

---
+++ 3.Prog_jazyky_a_vyvojove_prostredie/Ukážkové_zdrojáky/1.HelloWorld/HelloWorld/Source.cpp
@@ -0,0 +1,8 @@
+// Vloenie kninice pre  spustenie kódu
+#include <iostream>
+using namespace std;
+
+// Funkcia main ktorá musí by v KADOM C++ programe
+int main() {
+	cout << "Hello world!" << endl;
+}
